84d030daec25c033f74ab576811ddff33d51693d
Deilson Martins <45822038+deilsonmartins@users.noreply.github.com>
2020-08-17 22:44:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,57 @@
 import codecs
 import os
 
-from setuptools import setup
+from setuptools import find_packages, setup
+
+# get __version__ from _version.py
+ver_file = os.path.join('imblearn', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+DISTNAME = 'imbalanced-learn'
+DESCRIPTION = 'Toolbox for imbalanced dataset in machine learning.'
+with codecs.open('README.rst', encoding='utf-8-sig') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'G. Lemaitre, C. Aridas'
+MAINTAINER_EMAIL = 'g.lemaitre58@gmail.com, ichkoar@gmail.com'
+URL = 'https://github.com/scikit-learn-contrib/imbalanced-learn'
+LICENSE = 'MIT'
+DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/imbalanced-learn'
+VERSION = __version__
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: C',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8']
+INSTALL_REQUIRES = [
+    'numpy>=1.13.3',
+    'scipy>=0.19.1',
+    'scikit-learn>=0.23',
+    'joblib>=0.11'
+]
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest',
+        'pytest-cov'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'sphinxcontrib-bibtex',
+        'numpydoc',
+        'matplotlib',
+        'pandas',
+    ]
+}
 
 
 setup(
